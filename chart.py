c7e2d3a966db0a754d158a358fe979d35801240d
jaehyuk-lee-0712 <57936553+jaehyuk-lee-0712@users.noreply.github.com>
2024-04-26 12:49:43 +0900

    Create chart.py

---
+++ chart.py
@@ -0,0 +1,28 @@
+import requests as req 
+from bs4 import BeautifulSoup as bs
+import pandas as pd 
+
+res = req.get("https://music.bugs.co.kr/chart")
+
+soup = bs(res.text , 'lxml')
+
+ranking = soup.select(".ranking > strong")
+title = soup.select(".title > a")
+artist = soup.select(".artist >a:nth-child(1)")
+
+rankingList = []
+titleList = []
+artistList = []
+
+rankingList = [rank.text.strip() for rank in ranking]
+titleList = [title.text.strip() for title in title]
+artistList = [art.text.strip() for art in artist]
+
+chart_df = pd.DataFrame({
+    "Ranking" : rankingList,
+    "Title" : titleList , 
+    "Artist" : artistList
+})
+
+chart_df.to_json("bugsMusicChart100.json", force_ascii=False , orient="records")
+    
