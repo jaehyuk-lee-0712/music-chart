c3ec02ae3bcb7473929b30c2b5a9be0cff10fa3b
jaehyuk-lee-0712 <57936553+jaehyuk-lee-0712@users.noreply.github.com>
2024-04-29 10:46:16 +0900

    Update flo_chart.py

--- flo_chart.py
+++ flo_chart.py
@@ -1,11 +1,11 @@
 import time
+import selenium 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
-import selenium 
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
